1bede68e43dbff518af6b2e5679adb3a78d21c6b
James Robbs <jamesrobbs01@gmail.com>
2021-11-10 12:44:35 +0000

    seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  restaurant = Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.full_address)
+  puts "Created a restaurant named #{restaurant.name}"
+end
